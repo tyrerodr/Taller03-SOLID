e41ed4a6cd05414f3decd4759c4534f76a242c0c
Rafael Montalvo <rafamont@espol.edu.ec>
2020-06-24 10:37:18 -0500

    Single responsibility principle

---
+++ src/taller_solid/SRP/EmailSender.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller_solid.SRP;
+
+/**
+ *
+ * @author Rafael
+ */
+public class EmailSender {
+    void sendEmail(User user,String msg){
+        //envia correo al usuario
+        //se lleva el metodo sendEmail a una nueva clase
+        //para que tenga una unica resposabilidad, separada
+        //del login
+    }
+    
+}

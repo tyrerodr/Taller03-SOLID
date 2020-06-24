ab604c90a7e475c62d69e29ee6e6392cd15cef5c
Bryan Alava <bpalava@espol.edu.ec>
2020-06-24 09:40:53 -0500

    Creacion de proyecto :v

---
+++ src/taller_solid/OCP/OpenClosePrinciple.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller_solid.OCP;
+
+/**
+ *
+ * @author Bryan
+ */
+public class OpenClosePrinciple {
+    
+    public OpenClosePrinciple(){
+        
+    }
+    
+}
